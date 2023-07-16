85061ac81d308c901e9edc3d21183eee7db752d2
19082002 <deybristi08@gmail.com>
2023-07-16 12:40:24 +0530

    color change

--- src/component/algorithm.js
+++ src/component/algorithm.js
@@ -4,9 +4,9 @@ var val=250;
 
 
 //spped function
-export  function speed(){
+export function speed(){
      val=350-document.getElementById("speed").value; 
-    waitforme(val);
+     waitforme(val);
  }
   async function waitforme(val){
      return new Promise(resolve => {
@@ -30,18 +30,18 @@ export async function bubbleSort()
     var i, j,n=bar.length;
     for (i = 0; i < n - 1; i++){
         for (j = 0; j < n - i - 1; j++){
-            bar[j].style.background='red';
-            bar[j+1].style.background='yellow';
+            bar[j].style.background='rgb(217, 99, 247)';
+            bar[j+1].style.background='rgb(240, 246, 141)';
             if (parseInt(bar[j].style.height) > parseInt(bar[j+1].style.height)){
                await waitforme(val);
                 swap(j,j+1);
             }
-            bar[j].style.background='blue';
-            bar[j+1].style.background='blue';
+            bar[j].style.background='rgb(121, 200, 239)';
+            bar[j+1].style.background='rgb(121, 200, 239)';
         }
-        bar[bar.length-i-1].style.background="rgb(245, 132, 170)";
+        bar[bar.length-i-1].style.background="rgb(244, 170, 182)";
     }
-    bar[0].style.background="rgb(245, 132, 170)";
+    bar[0].style.background="rgb(244, 170, 182)";
     bl(true);
 }
 
@@ -62,19 +62,19 @@ async function merge(bar, l, m, r)
 
     for (var i = 0; i < n1; i++) {
        await waitforme(val);
-        bar[l+i].style.background="yellow";
+        bar[l+i].style.background="rgb(240, 246, 141)";
         L[i] = bar[l + i].style.height;}
     for (var i = 0; i< n2; i++) {
        await waitforme(val);
-        bar[m+1+i].style.background="orange";
+        bar[m+1+i].style.background="rgb(121, 221, 176)";
         R[i] = bar[m + 1 + i].style.height;}
 
     var i=0,j=0, k = l;
  
     while (i < n1 && j < n2) {
        await waitforme(val);
-        if((n1+n2)===bar.length)bar[k].style.background="rgb(245, 132, 170)";
-            else bar[k].style.background="pink";
+        if((n1+n2)===bar.length)bar[k].style.background="rgb(244, 170, 182)";
+            else bar[k].style.background="rgb(217, 99, 247)";
         if (parseInt(L[i])<= parseInt(R[j])) { 
             bar[k] .style.height= L[i];
             i++;
@@ -87,16 +87,16 @@ async function merge(bar, l, m, r)
     }
     while (i < n1) {
        await waitforme(val);
-        if((n1+n2)===bar.length)bar[k].style.background="rgb(245, 132, 170)";
-        else bar[k].style.background="pink";
+        if((n1+n2)===bar.length)bar[k].style.background="rgb(244, 170, 182)";
+        else bar[k].style.background="rgb(180, 170, 170)";
         bar[k].style.height= L[i];
         i++;
         k++;
     }
     while (j < n2) {
        await waitforme(val);
-        if((n1+n2)===bar.length)bar[k].style.background="rgb(245, 132, 170)";
-        else bar[k].style.background="pink";
+        if((n1+n2)===bar.length)bar[k].style.background="rgb(244, 170, 182)";
+        else bar[k].style.background="rgb(180, 170, 170)";
         bar[k].style.height= R[j];
         j++;
         k++;
@@ -121,24 +121,24 @@ export async function selectionSort(){
 
     for (i = 0; i < n-1; i++){
         min_idx = i;
-        bar[i].style.background="red";
+        bar[i].style.background="rgb(217, 99, 247)";
         for (j = i + 1; j < n; j++){     
-            bar[j].style.background="yellow"; 
+            bar[j].style.background="rgb(240, 246, 141)"; 
             await waitforme(val);
         if (parseInt(bar[j].style.height) < parseInt(bar[min_idx].style.height)){
             
             // await waitforme();
             if(min_idx !=i)
-            bar[min_idx].style.background="blue";
-            // bar[i].style.background="red";
+            bar[min_idx].style.background="rgb(121, 200, 239)";
+            // bar[i].style.background="rgb(217, 99, 247)";
             min_idx = j;
         }
-        else bar[j].style.background="blue"; 
+        else bar[j].style.background="rgb(121, 200, 239)"; 
     }
     await waitforme(val);    
         swap(min_idx, i);
-        bar[min_idx].style.background="blue";
-        bar[i].style.background="rgb(245, 132, 170)";
+        bar[min_idx].style.background="rgb(121, 200, 239)";
+        bar[i].style.background="rgb(244, 170, 182)";
     }
  
     bl(true);
@@ -148,22 +148,22 @@ export async function selectionSort(){
 export async function insertionSort() { 
     bl(false);
     let i, key, j,n=bar.length; 
-    bar[0].style.background="rgb(245, 132, 170)";
+    bar[0].style.background="rgb(244, 170, 182)";
     for (i = 1; i < n; i++) { 
         key = parseInt(bar[i].style.height); 
         j = i - 1; 
-        bar[i].style.background='red';
+        bar[i].style.background='rgb(217, 99, 247)';
         await waitforme(val);
-        // bar[j+1].style.background='yellow';
+        // bar[j+1].style.background='rgb(240, 246, 141)';
         while (j >= 0 && parseInt(bar[j].style.height) > key) { 
-            bar[j].style.background='red';
+            bar[j].style.background='rgb(217, 99, 247)';
             bar[j + 1].style.height = bar[j].style.height; 
             j = j - 1; 
             await waitforme(val);
-            for(let k=i;k>=0;k--)bar[k].style.background="rgb(245, 132, 170)";
+            for(let k=i;k>=0;k--)bar[k].style.background="rgb(244, 170, 182)";
         } 
         bar[j + 1].style.height= key; 
-        bar[i].style.background="rgb(245, 132, 170)";
+        bar[i].style.background="rgb(244, 170, 182)";
     } 
     bl(true);
 } 
@@ -174,19 +174,19 @@ async function partition(bar, left, right) {
     // var temp;
      var i=left-1;
    for( let j=left;j<=right-1;j++){
-    bar[j].style.background="yellow";
+    bar[j].style.background="rgb(240, 246, 141)";
     await waitforme(val);
     if(parseInt(bar[j].style.height)<parseInt( bar[right].style.height)){
         i++;   
         let temp=bar[j].style.height;
    bar[j].style.height=bar[i].style.height;
    bar[i].style.height=temp; 
-        bar[i].style.background="orange";
-        if(i!=j) bar[j].style.background="orange";
+        bar[i].style.background="rgb(121, 221, 176)";
+        if(i!=j) bar[j].style.background="rgb(121, 221, 176)";
         await waitforme(val);
     }
     else{
-        bar[j].style.background="pink";
+        bar[j].style.background="rgb(217, 99, 247)";
     }
    }
    i++;
@@ -194,11 +194,11 @@ async function partition(bar, left, right) {
    let temp=bar[right].style.height;
    bar[right].style.height=bar[i].style.height;
    bar[i].style.height=temp;   
-   bar[right].style.background="pink";
-   bar[i].style.background="rgb(245, 132, 170)";
+   bar[right].style.background="rgb(217, 99, 247)";
+   bar[i].style.background="rgb(244, 170, 182)";
    await waitforme(val);
    for(let k=0;k<bar.length;k++){
-    if(bar[k].style.background!="rgb(245, 132, 170)")bar[k].style.background="blue";
+    if(bar[k].style.background!="rgb(244, 170, 182)")bar[k].style.background="rgb(121, 200, 239)";
    }
     return i;
 }
@@ -210,8 +210,8 @@ async function quickSort(bar, low,high) {
   }
   else{
     if(low>=0 && high>=0 && low<bar.length && high<bar.length ){
-        bar[low].style.background="rgb(245, 132, 170)";
-        bar[high].style.background="rgb(245, 132, 170)";
+        bar[low].style.background="rgb(244, 170, 182)";
+        bar[high].style.background="rgb(244, 170, 182)";
     }
   }
     // return bar;
